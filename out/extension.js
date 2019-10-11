f3c1fbf54a1b1d14ed3430f677fc17e0478b8599
eplak <christoffer.bjelke@outlook.com>
2019-10-11 15:41:56 +0200

    It now does not change the keyword if it is part of a word

---
+++ out/extension.js
@@ -0,0 +1,65 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('extension.SQLuppercase', () => {
+        // The code you place here will be executed every time your command is executed
+        let SQLKeyWords = ["select", "from", "where", "inner", "natural", "join", "outer", "right", "left", "full", "having", "as", "create", "view", "is", "null", "on", "using", "count", "not", "like", "and", "or", "order", "by", "group", "desc", "union", "with", "distinct", "add", "constraint", "alter", "coloumn", "table", "all", "any", "asc", "database", "between", "case", "check", "index", "replace", "procedure", "unique", "default", "delete", "drop", "exec", "exists", "foreign", "from", "in", "insert", "into", "limit", "primary", "key", "rownum", "set", "top", "trunctate", "update", "values"];
+        let editor = vscode.window.activeTextEditor;
+        if (editor != undefined) {
+            let document = editor.document;
+            let text = document.getText();
+            let lineArray = [];
+            let newLineArray = [];
+            for (let index = 0; index < document.lineCount; index++) {
+                lineArray.push(document.lineAt(index));
+            }
+            for (let index = 0; index < lineArray.length; index++) {
+                let lineText = lineArray[index].text;
+                let textArray = lineText.split(" ");
+                let newTextArray = [];
+                for (let index2 = 0; index2 < textArray.length; index2++) {
+                    const word = textArray[index2];
+                    if (SQLKeyWords.includes(word)) {
+                        newTextArray.push(word.toUpperCase());
+                    }
+                    else {
+                        newTextArray.push(word);
+                    }
+                }
+                let newText = "";
+                for (let index2 = 0; index2 < newTextArray.length; index2++) {
+                    const word = newTextArray[index2];
+                    newText += word + " ";
+                }
+                newText = newText.slice(0, newText.length - 1);
+                newText += "\n";
+                newLineArray.push(newText);
+            }
+            let newText = "";
+            for (let index = 0; index < newLineArray.length; index++) {
+                newText += newLineArray[index];
+            }
+            newText = newText.slice(0, newText.length - 1);
+            //deletes old text and inserts new
+            editor.edit(editBuilder => {
+                editBuilder.delete(new vscode.Range(document.positionAt(0), document.positionAt(text.length)));
+                let beginning = new vscode.Position(0, 0);
+                editBuilder.insert(beginning, newText);
+            });
+        }
+    });
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -5,19 +5,68 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-uppercasesql" is now active!');
-
+	
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable : vscode.Disposable = vscode.commands.registerCommand('extension.SQLuppercase', () => {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		let SQLKeyWords : string[] = ["select","from","where","inner","natural","join","outer","right","left","full","having","as","create","view","is","null","on","using","count","not","like","and","or","order","by","group","desc","union","with","distinct","add","constraint","alter","coloumn","table","all","any","asc","database","between","case","check","index","replace","procedure","unique","default","delete","drop","exec","exists","foreign","from","in","insert","into","limit","primary","key","rownum","set","top","trunctate","update","values"]
+
+		let editor : vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+
+		if (editor != undefined) {
+			let document : vscode.TextDocument = editor.document;
+			let text : string = document.getText();
+			let lineArray : vscode.TextLine[] = [];
+			let newLineArray : string[] = [];
+
+			for (let index = 0; index < document.lineCount; index++) {
+				lineArray.push(document.lineAt(index));
+			}
+
+			for (let index = 0; index < lineArray.length; index++) {
+				let lineText : string = lineArray[index].text;
+				let textArray : string[] = lineText.split(" ");
+				let newTextArray : string[] = [];
+
+				for (let index2 = 0; index2 < textArray.length; index2++) {
+					const word : string = textArray[index2];
+					if (SQLKeyWords.includes(word)) {
+						newTextArray.push(word.toUpperCase());
+					} else {
+						newTextArray.push(word);
+					}
+				}
+
+				let newText : string = "";
+				for (let index2 = 0; index2 < newTextArray.length; index2++) {
+					const word : string = newTextArray[index2]
+					newText += word + " ";
+				}
+				newText = newText.slice(0,newText.length-1);
+				newText += "\n";
+				newLineArray.push(newText);
+			}
+
+			let newText : string = "";
+			
+			for (let index = 0; index < newLineArray.length; index++) {
+				newText += newLineArray[index];
+				
+			}
+			newText = newText.slice(0,newText.length-1);
+
+			//deletes old text and inserts new
+			editor.edit(editBuilder => {
+				editBuilder.delete(new vscode.Range(document.positionAt(0), document.positionAt(text.length)));
+				let beginning : vscode.Position = new vscode.Position(0, 0);
+				editBuilder.insert(beginning, newText);
+			});
+
+		}
+
 	});
 
 	context.subscriptions.push(disposable);
